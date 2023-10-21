f1071ba3ed5e956d7a94a3255607f3b578e621f4
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-10-21 18:42:16 -0300

    Creation of service and repositories

---
+++ src/Services/BookingServices.js
@@ -0,0 +1,19 @@
+const Booking = require('../bookings/Booking')
+
+class BookingService{
+    constructor(repository){
+        this.repository = repository
+    }
+
+    findAllBookings(){
+        return  this.repository.findAll()
+    }
+
+    createBooking({ roomId, guestName, checkInDate, checkOutDate }){
+        const newBooking = new Booking(roomId, guestName, checkInDate, checkOutDate )
+        this.repository.create(newBooking)
+        return newBooking
+    }
+}
+
+module.exports = BookingService
